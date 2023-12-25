e394538b1376a4cb5addab5e71e6c83ea0bd5ff2
Durgesh183 <152973665+Durgesh183@users.noreply.github.com>
2023-12-25 21:17:15 +0530

    3.c

---
+++ 3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++){
+        sum=sum+i;
+    }
+    
+        
+
+    
+    printf("The sum is:%d",sum);
+}
